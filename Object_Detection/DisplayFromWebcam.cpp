a57d1d9729e683542454a934e63d748127065c7b
Ashwin Shenai <ashwins@iitk.ac.in>
2019-03-13 00:08:13 +0530

    changed exit key

--- Object_Detection/DisplayFromWebcam.cpp
+++ Object_Detection/DisplayFromWebcam.cpp
@@ -3,13 +3,13 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
-    VideoCapture cap(0);
+    VideoCapture cap("rtsp://192.168.42.1/live");
     Mat frame;
 A:
     cap >> frame;
     imshow("MuGsHoT", frame);
-    // 24 ms refresh rate, press 'A' to exit
-    if ((char)waitKey(24) == 'a')
+    // 24 ms refresh rate, press 'Q' to exit
+    if ((char)waitKey(24) == 'q')
     {
         return 0;
     }
